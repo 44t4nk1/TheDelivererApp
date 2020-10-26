331c1af0a10eb2b213fa70b53c0839d9f9fea4e8
Siddharth Singh <siddharthsingh3700@gmail.com>
2020-10-26 21:01:28 +0530

    dybnamic total price

--- lib/screens/orderPage.dart
+++ lib/screens/orderPage.dart
@@ -26,118 +26,164 @@ class _OrderPageState extends State<OrderPage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).accentColor,
-          title: Text(
-            widget.name,
-            style: GoogleFonts.montserrat(
-              fontSize: 18,
-              fontWeight: FontWeight.w800,
-              color: Colors.white,
-            ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).accentColor,
+        title: Text(
+          widget.name,
+          style: GoogleFonts.montserrat(
+            fontSize: 18,
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
           ),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              for (var item in widget.items)
-                Container(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: size.width * 35 / 100,
-                            margin: EdgeInsets.only(
-                                top: size.height * 2 / 100,
-                                left: size.width * 6 / 100),
-                            child: Text(
-                              item["name"],
-                              style: GoogleFonts.montserrat(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w700,
-                                fontSize: 20,
-                              ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            for (var item in widget.items)
+              Container(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: size.width * 35 / 100,
+                          margin: EdgeInsets.only(
+                              top: size.height * 2 / 100,
+                              left: size.width * 6 / 100),
+                          child: Text(
+                            item["name"],
+                            style: GoogleFonts.montserrat(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 20,
                             ),
                           ),
-                          Container(
-                            width: size.width * 45 / 100,
-                            margin: EdgeInsets.only(
-                                top: size.height * 0.2 / 100,
-                                left: size.width * 6 / 100),
-                            child: Text(
-                              item["description"],
-                              style: GoogleFonts.montserrat(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w400,
-                                fontSize: 16,
-                              ),
+                        ),
+                        Container(
+                          width: size.width * 45 / 100,
+                          margin: EdgeInsets.only(
+                              top: size.height * 0.2 / 100,
+                              left: size.width * 6 / 100),
+                          child: Text(
+                            item["description"],
+                            style: GoogleFonts.montserrat(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 16,
                             ),
                           ),
-                        ],
-                      ),
-                      Spacer(),
-                      Container(
-                        margin: EdgeInsets.only(
-                          right: size.width * 1 / 100,
-                          top: size.height * 2 / 100,
                         ),
-                        child: Text(
-                          '₹' + item["price"],
-                          style: GoogleFonts.montserrat(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 20,
-                          ),
-                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    Container(
+                      margin: EdgeInsets.only(
+                        right: size.width * 1 / 100,
+                        top: size.height * 2 / 100,
                       ),
-                      IconButton(
-                        icon: Icon(
-                          FontAwesomeIcons.minus,
-                          color: Color(0xfff55a42),
-                          size: 18,
-                        ),
-                        onPressed: () {
-                          if (int.parse(item["quantity"]) > 0) {
-                            setState(() {
-                              item["quantity"] =
-                                  (int.parse(item["quantity"]) - 1).toString();
-                            });
-                          }
-                        },
-                      ),
-                      Container(
-                        width: size.width * 2 / 100,
-                        margin: EdgeInsets.only(top: size.height * 2 / 100),
-                        child: Text(
-                          item["quantity"],
-                          style: GoogleFonts.montserrat(
-                            color: Colors.black,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 20,
-                          ),
+                      child: Text(
+                        '₹' + item["price"],
+                        style: GoogleFonts.montserrat(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
                         ),
                       ),
-                      IconButton(
-                        icon: Icon(
-                          FontAwesomeIcons.plus,
-                          color: Color(0xff42f59e),
-                          size: 18,
-                        ),
-                        onPressed: () {
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        FontAwesomeIcons.minus,
+                        color: Color(0xfff55a42),
+                        size: 18,
+                      ),
+                      onPressed: () {
+                        if (int.parse(item["quantity"]) > 0) {
                           setState(() {
                             item["quantity"] =
-                                (int.parse(item["quantity"]) + 1).toString();
+                                (int.parse(item["quantity"]) - 1).toString();
+                            widget.total = 0;
+                            for (var i in widget.items) {
+                              widget.total = widget.total +
+                                  (int.parse(i["quantity"]) *
+                                      int.parse(i["price"]));
+                            }
                           });
-                        },
+                        }
+                      },
+                    ),
+                    Container(
+                      width: size.width * 2 / 100,
+                      margin: EdgeInsets.only(top: size.height * 2 / 100),
+                      child: Text(
+                        item["quantity"],
+                        style: GoogleFonts.montserrat(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        FontAwesomeIcons.plus,
+                        color: Color(0xff42f59e),
+                        size: 18,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          item["quantity"] =
+                              (int.parse(item["quantity"]) + 1).toString();
+                          widget.total = 0;
+                          for (var i in widget.items) {
+                            widget.total = widget.total +
+                                (int.parse(i["quantity"]) *
+                                    int.parse(i["price"]));
+                          }
+                        });
+                      },
+                    ),
+                  ],
                 ),
-            ],
-          ),
-        ));
+              ),
+            SizedBox(
+              height: size.height * 2 / 100,
+            ),
+            Container(
+              height: 0.3,
+              margin: EdgeInsets.symmetric(horizontal: size.width * 4 / 100),
+              color: Theme.of(context).backgroundColor,
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                  top: size.height * 2 / 100, left: size.width * 6 / 100),
+              child: Text(
+                "Your total amount",
+                style: GoogleFonts.montserrat(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                  top: size.height * 1 / 100, left: size.width * 6 / 100),
+              child: Text(
+                '₹ ' + widget.total.toString(),
+                style: GoogleFonts.montserrat(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
