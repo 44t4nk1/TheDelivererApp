932055e0f56b9dc58e0ce7585fb35e00b9ba5fde
Siddharth Singh <siddharthsingh3700@gmail.com>
2020-11-01 16:07:02 +0530

    add ress

--- lib/screens/landingPage.dart
+++ lib/screens/landingPage.dart
@@ -2,12 +2,15 @@ import 'package:TheDeliverer/animations/BounceIn.dart';
 import 'package:TheDeliverer/main.dart';
 import 'package:TheDeliverer/providers/reg.dart';
 import 'package:TheDeliverer/screens/foodCard.dart';
+import 'package:TheDeliverer/providers/userDetails.dart';
 import 'package:TheDeliverer/screens/orderPage.dart';
 import 'package:TheDeliverer/screens/restaurantCard.dart';
+import 'package:TheDeliverer/screens/selectAddress.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'globals.dart' as globals;
 import 'package:provider/provider.dart';
 
 class LandingPage extends StatefulWidget {
@@ -16,6 +19,23 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
+  void initState() {
+    super.initState();
+    visitScreen();
+  }
+
+  bool isLoading = false;
+  Future<void> visitScreen() async {
+    setState(() {
+      isLoading = true;
+    });
+    await Provider.of<User>(context, listen: false)
+        .fetchDetails(Provider.of<Reg>(context, listen: false).token);
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   final List specialOffers = [
     {
       "name": "Tandoori Momos",
@@ -99,162 +119,184 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Stack(
-      children: [
-        SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return isLoading
+        ? Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        : Stack(
             children: [
-              SizedBox(
-                height: size.height * 15.5 / 100,
-              ),
-              Container(
-                padding: EdgeInsets.only(left: size.width * 4 / 100),
-                child: Text(
-                  "Our best Offers!",
-                  style: GoogleFonts.montserrat(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w800,
-                    color: Theme.of(context).backgroundColor,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: size.height * 2 / 100,
-              ),
-              Container(
-                height: size.height * 20 / 100,
-                width: double.maxFinite,
-                child: ListView.builder(
-                  shrinkWrap: false,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: specialOffers.length,
-                  padding:
-                      EdgeInsets.symmetric(horizontal: size.width * 4 / 100),
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: EdgeInsets.only(right: size.width * 4 / 100),
-                      child: FoodCard(
-                        name: specialOffers[index]['name'],
-                        description: specialOffers[index]['description'],
-                        price: specialOffers[index]['price'],
+              SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: size.height * 15.5 / 100,
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: size.width * 4 / 100),
+                      child: Text(
+                        "Our best Offers!",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w800,
+                          color: Theme.of(context).backgroundColor,
+                        ),
                       ),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(
-                height: size.height * 2 / 100,
-              ),
-              Container(
-                padding: EdgeInsets.only(left: size.width * 4 / 100),
-                child: Text(
-                  "Our favourite Restaurants!",
-                  style: GoogleFonts.montserrat(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w800,
-                    color: Theme.of(context).backgroundColor,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: size.height * 2 / 100,
-              ),
-              Container(
-                height: size.height * 20 / 100,
-                width: double.maxFinite,
-                child: ListView.builder(
-                  shrinkWrap: false,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: restaurants.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      child: FlatButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            BounceIn(
-                              widget: OrderPage(
+                    ),
+                    SizedBox(
+                      height: size.height * 2 / 100,
+                    ),
+                    Container(
+                      height: size.height * 20 / 100,
+                      width: double.maxFinite,
+                      child: ListView.builder(
+                        shrinkWrap: false,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: specialOffers.length,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: size.width * 4 / 100),
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin:
+                                EdgeInsets.only(right: size.width * 4 / 100),
+                            child: FoodCard(
+                              name: specialOffers[index]['name'],
+                              description: specialOffers[index]['description'],
+                              price: specialOffers[index]['price'],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: size.height * 2 / 100,
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: size.width * 4 / 100),
+                      child: Text(
+                        "Our favourite Restaurants!",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w800,
+                          color: Theme.of(context).backgroundColor,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: size.height * 2 / 100,
+                    ),
+                    Container(
+                      height: size.height * 20 / 100,
+                      width: double.maxFinite,
+                      child: ListView.builder(
+                        shrinkWrap: false,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: restaurants.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            child: FlatButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  BounceIn(
+                                    widget: OrderPage(
+                                      name: restaurants[index]['name'],
+                                      items: restaurants[index]['items'],
+                                    ),
+                                  ),
+                                );
+                              },
+                              child: RestaurantCard(
                                 name: restaurants[index]['name'],
-                                items: restaurants[index]['items'],
+                                address: restaurants[index]['address'],
+                                distance: restaurants[index]['distance'],
                               ),
                             ),
                           );
                         },
-                        child: RestaurantCard(
-                          name: restaurants[index]['name'],
-                          address: restaurants[index]['address'],
-                          distance: restaurants[index]['distance'],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: size.height * 4.5 / 100,
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: size.width * 4 / 100),
+                      child: Text(
+                        "Now Delivering to",
+                        style: GoogleFonts.montserrat(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          color: Theme.of(context).backgroundColor,
                         ),
                       ),
-                    );
-                  },
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        FlatButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              BounceIn(
+                                widget: SelectAddressScreen(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            child: Text(
+                              globals.currentAddress.isEmpty
+                                  ? "Select an Address"
+                                  : globals.currentAddress["city"] +
+                                      ', ' +
+                                      globals.currentAddress["state"],
+                              style: GoogleFonts.montserrat(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w800,
+                                color: Theme.of(context).accentColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Spacer(),
+                        Container(
+                          padding: EdgeInsets.only(right: size.width * 4 / 100),
+                          child: IconButton(
+                            icon: Icon(
+                              FontAwesomeIcons.signOutAlt,
+                              size: 20,
+                              color: Theme.of(context).accentColor,
+                            ),
+                            onPressed: () async {
+                              await Provider.of<Reg>(context, listen: false)
+                                  .logout();
+                              RestartWidget.restartApp(context);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 0.3,
+                      width: double.infinity,
+                      color: Theme.of(context).backgroundColor,
+                      margin: EdgeInsets.symmetric(
+                          horizontal: size.width * 4 / 100),
+                    ),
+                  ],
                 ),
               ),
             ],
-          ),
-        ),
-        Container(
-          color: Colors.white,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: size.height * 4.5 / 100,
-              ),
-              Container(
-                padding: EdgeInsets.only(left: size.width * 4 / 100),
-                child: Text(
-                  "Now Delivering to",
-                  style: GoogleFonts.montserrat(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
-                    color: Theme.of(context).backgroundColor,
-                  ),
-                ),
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(left: size.width * 4 / 100),
-                    child: Text(
-                      "Rajkot, Gujarat",
-                      style: GoogleFonts.montserrat(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w800,
-                        color: Theme.of(context).accentColor,
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    padding: EdgeInsets.only(right: size.width * 4 / 100),
-                    child: IconButton(
-                      icon: Icon(
-                        FontAwesomeIcons.signOutAlt,
-                        size: 20,
-                        color: Theme.of(context).accentColor,
-                      ),
-                      onPressed: () async {
-                        await Provider.of<Reg>(context, listen: false).logout();
-                        RestartWidget.restartApp(context);
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              Container(
-                height: 0.3,
-                width: double.infinity,
-                color: Theme.of(context).backgroundColor,
-                margin: EdgeInsets.symmetric(horizontal: size.width * 4 / 100),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
+          );
   }
 }
---
+++ lib/screens/selectAddress.dart
@@ -0,0 +1,90 @@
+import 'package:TheDeliverer/providers/userDetails.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class SelectAddressScreen extends StatefulWidget {
+  @override
+  _SelectAddressScreenState createState() => _SelectAddressScreenState();
+}
+
+class _SelectAddressScreenState extends State<SelectAddressScreen> {
+  @override
+  void initState() {
+    super.initState();
+    visitScreen();
+  }
+
+  Map userDetails;
+  List<dynamic> addresses;
+  Future<void> visitScreen() async {
+    userDetails = Provider.of<User>(context, listen: false).userDetails;
+    addresses = userDetails["UserAddresses"];
+    print(addresses);
+  }
+
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).accentColor,
+        title: Text(
+          "Select an Address",
+          style: GoogleFonts.montserrat(
+            fontSize: 18,
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: size.height * 1.5 / 100,
+          ),
+          for (var i in addresses)
+            Container(
+              margin: EdgeInsets.only(bottom: size.height * 1 / 100),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(left: size.width * 4 / 100),
+                    child: Icon(
+                      FontAwesomeIcons.home,
+                      color: Theme.of(context).accentColor,
+                    ),
+                  ),
+                  Flexible(
+                    child: Container(
+                      margin: EdgeInsets.only(
+                        left: size.width * 4 / 100,
+                        right: size.width * 4 / 100,
+                      ),
+                      child: Text(
+                        i["addressLine1"] +
+                            ', ' +
+                            i["addressLine2"] +
+                            ', ' +
+                            i["city"] +
+                            ', ' +
+                            i["state"] +
+                            ', ' +
+                            i["pinCode"].toString(),
+                        style: GoogleFonts.montserrat(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                          color: Theme.of(context).backgroundColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
